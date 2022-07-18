5120e273046c8b5c05b1916318962937f0731e9a
Thiago González <thiagogonzalez2000@gmail.com>
2022-07-18 10:58:36 -0300

    feature/implement-signin-page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import Routes from './routes';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer 
+        autoClose={3000}
+      />
       <Routes />
     </BrowserRouter>
   );
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,41 @@
+import { toast } from "react-toastify";
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+import './login.css';
+
+import basketball from '../../assets/basketball.png';
+
 export const SignIn = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSumbit(e) {
+        e.preventDefault();
+
+        if (email !== '' && password !== '') {
+            toast.success('Form submetido com sucesso!');
+        } else if (email === '' || password === '') {
+            toast.warning('Preencha todos os campos!');
+        }
+    }
+
     return (
-        <h1>SignIn Page</h1>
+        <div className="sign-page">
+            <div className="sign">
+                <div className="sign-area">
+                    <img src={basketball} alt="Imagem ilustrativa de uma cesta de basquete" />
+                </div>
+
+                <form onSubmit={handleSumbit}>
+                    <h1>Acesse myGameScore</h1>
+                    <input type="text" placeholder="Email" value={email} onChange={ (e) => setEmail(e.target.value)} />
+                    <input type="password" placeholder="Senha" value={password} onChange={ (e) => setPassword(e.target.value)} />
+                    <button className='btn' type="submit">Acessar</button>
+                </form>
+
+                <Link className="register-btn" to="/register">Ainda não possui uma conta? Cadastre-se</Link>
+            </div>
+        </div>
     );
 }
