6173cfc998941aa2e7e69b13679bb167154b9e13
Thiago González <thiagogonzalez2000@gmail.com>
2022-07-18 13:35:37 -0300

    feature/implement-notfoundpage-and-change-loginpage-colors

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,5 +1,19 @@
+import { Container } from "react-bootstrap";
+
+import crybasketball from '../../assets/crybasketball.png';
+
+import './notfound.css';
+
 export const NotFound = () => {
     return(
-        <h1>NotFound page</h1>
+        <Container fluid className="notfound-page">
+            <div className="info">
+                <img className="crybasketball" src={crybasketball} alt="Imagem ilustrativa de uma bola de basquete triste" />
+                <div className="info-data">
+                    <h1>Ooops, página não encontrada!</h1>
+                    <p>Para retornar a uma rota válida, utilize o menu de navegação no topo da página</p>
+                </div>
+            </div>
+        </Container>
     );
 }
